916ef7dceb9b748b4e3647e863589d66bb52b5e7
Edson Finamori <edsonfi@uol.com.br>
2022-06-28 16:38:36 -0300

    Listando categorias - Parcial

---
+++ src/config/api.config.ts
@@ -0,0 +1,13 @@
+export const API_CONFIG = {
+    baseUrl: "http://localhost:8081"
+}
+
+/*export const API_CONFIG = {
+    //baseUrl: "http://localhost:8080"
+    baseUrl: @crossorigin(origins = "http://localhost:8100")
+}*/
+
+/*export const API_CONFIG = {
+    baseUrl: @crossOriginIsolated("http://localhost:8100);
+
+}*/
---
+++ src/services/domain/categoria.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { API_CONFIG } from "../../config/api.config";
+import { Observable } from "rxjs/Rx";
+import { CategoriaDTO } from "../../models/categoria.dto";
+
+@Injectable()
+export class CategoriaService{
+
+    constructor(public http: HttpClient) {
+    }
+
+    findAll() : Observable<CategoriaDTO[]> {
+        return this.http.get<CategoriaDTO[]>('${API_CONFIG.baseUrl}/categorias');
+    }
+}
+        //${API_CONFIG.baseUrl}
+        //http://localhost:8081
